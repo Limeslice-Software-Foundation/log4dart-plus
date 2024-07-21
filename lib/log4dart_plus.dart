bf27eb925c03cba8269cdc05ca9e806ce0a64e8a
trevorcharlesmiller <tcmiller.dev@proton.me>
2024-07-21 11:48:50 +1000

    0.0.2 Add Level class

--- lib/log4dart_plus.dart
+++ lib/log4dart_plus.dart
@@ -1,8 +1,21 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
+// Licensed to the Limeslice Software Foundation (LSF) under one or more
+// contributor license agreements.  See the NOTICE file distributed with
+// this work for additional information regarding copyright ownership.
+// The LSF licenses this file to You under the MIT License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://limeslice.org/license.txt
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/// Log4Dart Plus is a versatile and highly configurable logging framework
+/// for Dart / Flutter applications to assist the deployment of logging for
+/// various use cases.
 library;
 
-export 'src/log4dart_plus_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/level.dart';
